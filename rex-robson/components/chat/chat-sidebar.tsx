31cc556964a5e4375e0591f96f77e008f7847ebc
Adam <adam@robson.capital>
2026-04-17 12:28:58 +0100

    Changed 'members' back to 'contacts'

--- rex-robson/components/chat/chat-sidebar.tsx
+++ rex-robson/components/chat/chat-sidebar.tsx
@@ -12,7 +12,7 @@ import {
 
 const navItems = [
   { id: "ask", label: "Ask Rex", icon: MessageCircle },
-  { id: "contacts", label: "Members", icon: Users },
+  { id: "contacts", label: "Contacts", icon: Users },
   { id: "organisations", label: "Organisations", icon: Building2 },
   { id: "deal-canvas", label: "Deal Canvas", icon: LayoutGrid },
   { id: "tasks", label: "Tasks", icon: ListTodo },
